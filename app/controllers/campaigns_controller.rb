5f115b4cb07c2b9695673d5aac0c3e9ad570c175
shahin1290 <s.patowary@yahoo.com>
2018-12-05 15:09:44 +0100

    Adds sad path into feature, modifies form and conreoller to show the error messages

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -4,13 +4,17 @@ class CampaignsController < ApplicationController
   end
 
   def new
-
+    @campaign = Campaign.new
   end
 
   def create
     @campaign = Campaign.new(params.require(:campaign).permit(:title, :description, :amount_raised))
     @campaign.amount_raised = 0
-    @campaign.save
-    redirect_to campaigns_path
+    if @campaign.save
+      redirect_to campaigns_path
+    else
+      render 'new'
+    end
   end
+  
 end
