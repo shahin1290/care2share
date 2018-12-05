595535958e65a4aaab13ce9e35ffc78cf03d2976
shahin1290 <s.patowary@yahoo.com>
2018-12-05 20:10:42 +0100

    Updates controller, adds show page to show the campaign details

--- features/step_definitions/asstertion_stpes.rb
+++
@@ -1,8 +0,0 @@
-Then("I should see {string}") do |content|
-    expect(page).to have_content content
-end
-  
-Then("I should be on the campaign details page") do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
