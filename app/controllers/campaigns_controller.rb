bb9f606bf43242816b73b9b6cc13c2d6b5027e33
shahin1290 <s.patowary@yahoo.com>
2018-12-06 16:38:25 +0100

    Adds edit, delete

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -10,6 +10,10 @@ class CampaignsController < ApplicationController
     @campaign = Campaign.new
   end
 
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
   def create
     @campaign = Campaign.new(params.require(:campaign).permit(:title, :description, :amount_raised))
     @campaign.amount_raised = 0
@@ -18,6 +22,23 @@ class CampaignsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+   
+    if @campaign.update(params.require(:campaign).permit(:title, :description, :amount_raised))
+      redirect_to @campaign
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    @campaign.destroy
+ 
+    redirect_to campaigns_path
   end
   
 end
