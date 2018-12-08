8c3a6c750f859ef8c42c3d5aafaaab6f88873854
shahin1290 <s.patowary@yahoo.com>
2018-12-08 19:25:44 +0100

    Updates routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :campaigns, only: [:index, :new, :create, :show]
+  resources :campaigns
   resources :donations, only: [:new, :create]
   root controller: :campaigns, action: :index
 end
